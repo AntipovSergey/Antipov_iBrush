23576c1916d5fe390eb471eccd4cf1dcae8bffe4
Sergey Antipov <72650361+AntipovSergey@users.noreply.github.com>
2021-08-24 14:37:58 +0300

    Завершает верстку, стилизацию и адаптацию задания

--- source/js/script.js
+++ source/js/script.js
@@ -14,3 +14,23 @@ let options = document.querySelectorAll('.choices__item');
 choices.addEventListener('click', () => {
     options[3].classList.add('is-selected');
 })
+
+// Слайдер
+
+const swiper = document.querySelector('.swiper-container')
+
+const mySwiper = new Swiper(swiper, {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
+});
